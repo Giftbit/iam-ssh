a4dceb468b25381e8e9f3f65670c45f7046510cf
Jamie Starke <jamies@giftbit.com>
2016-09-30 12:59:59 -0700

    Dynamically grab the region of the instance

--- authorized_groups.sh
+++ authorized_groups.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-aws --region=us-west-2 ec2 describe-tags --filters "Name=resource-id,Values=$(curl http://169.254.169.254/latest/meta-data/instance-id)" "Name=key,Values=AuthorizedUsersGroup" --query "Tags[].Value" --output text | tr "," "\n"
+REGION=`curl http://169.254.169.254/latest/dynamic/instance-identity/document|grep region|awk -F\" '{print $4}'`
+
+aws --region=$REGION ec2 describe-tags --filters "Name=resource-id,Values=$(curl http://169.254.169.254/latest/meta-data/instance-id)" "Name=key,Values=AuthorizedUsersGroup" --query "Tags[].Value" --output text | tr "," "\n"
