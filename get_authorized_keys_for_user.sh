b957adfa0a3f58d98f94ed3aacdeffbccc4eb4e1
Jamie Starke <jamies@giftbit.com>
2016-09-30 13:10:15 -0700

    Updated the install location for the scripts, and updated the indentation

--- get_authorized_keys_for_user.sh
+++ get_authorized_keys_for_user.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -e
 username=$1
 
-if /usr/local/bin/get_authorized_users.sh | grep "^$username$"; then
+if /opt/iam_ssh/get_authorized_users.sh | grep "^$username$" >/dev/null 2>&1; then
   aws iam list-ssh-public-keys --user-name $username --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read ssh_key_id; do
-  	aws iam get-ssh-public-key --user-name $username --ssh-public-key-id $ssh_key_id --query "SSHPublicKey.SSHPublicKeyBody" --encoding ssh --output txt
+    aws iam get-ssh-public-key --user-name $username --ssh-public-key-id $ssh_key_id --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
   done
 fi
