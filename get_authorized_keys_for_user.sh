b2884d395ca564ed972ac7afb8c72811db7cfa92
Jamie Starke <jamies@giftbit.com>
2016-10-24 11:05:39 -0700

    Added some commenting about what is happening in each of the functions

--- get_authorized_keys_for_user.sh
+++ get_authorized_keys_for_user.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -e
+
+# This script verifies that the requested user is in the list of authorized users, 
+# and if they are, retrieves the list of public keys associated with them.
+#
+# If the user is not authorized or has no public keys, this script will return no
+# public keys and thus SSH will have no public keys to match, rejecting the request
+# to connect.
+
 username=$1
 
+# Verify that the user attempting to connect is in the authorized groups
 if /opt/iam_ssh/get_authorized_users.sh | grep "^$username$" >/dev/null 2>&1; then
+  
+  # Get the list of Public Keys for the authorized user, if any
   aws iam list-ssh-public-keys --user-name $username --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read ssh_key_id; do
     aws iam get-ssh-public-key --user-name $username --ssh-public-key-id $ssh_key_id --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
   done
--- get_authorized_users.sh
+++ get_authorized_users.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+# This script gets the authorized groups for this instance, and for each one, retrieves
+# the list of users associated with the named groups. Finally, they are sorted and 
+# duplicates are removed
+
 /opt/iam_ssh/authorized_groups.sh | xargs -L 1 -I '{}' aws iam get-group --group-name '{}' --query "Users[].[UserName]" --output text | sort | uniq
